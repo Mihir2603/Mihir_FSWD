b00b748c31031392b760ea746e1e52a6a09982ee
Mihir patel <patelmihir263@gmail.com>
2022-07-05 10:48:51 +0530

    object

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("Hey Mihir");
+const obj={
+    name:"mkem cho?",
+    brand:"super cool",
+    model:"2002",
+    log:()=>{console.log('my name is mihir')}
+
+}
+console.log(obj);
+console.log(typeof obj)
+
+console.log('log');
+console.error('error');
+console.warn('warn');
+console.date=function(){
+console.log(new Date)
+}
+// console.log(window)
+// window.alert("this is my alert")
+// window.alert('this is an alert')
+// console.date=function(){
+// console.log(new Date())
+// }
+// console.log(Window.innerheight)
+// console.log(window.innerWidth)
+
+// const name=window.prompt("What is your name?")
+// console.log(name);
+
+// window.open("https://www.youtube.com","_blank");
+console.log(window.location);
